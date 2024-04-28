fe209aefb1b07fc3de067a3c399a5c527c1083c0
landroide <landrade1313@gmail.com>
2024-04-28 18:58:11 +1200

    tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
  
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'places',
     pathMatch: 'full'
   },
   {
--- src/app/pages/places/places-routing.module.ts
+++ src/app/pages/places/places-routing.module.ts
@@ -6,16 +6,54 @@ import { PlacesPage } from './places.page';
 const routes: Routes = [
   {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
+    component: PlacesPage,
+    children:[
+    {
+      path: 'discover',
+      children: [
+        {
+          path: '',
+          loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+        },
+        {
+          path:':placeId',
+          loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+        },
+        {
+          path: '',
+          redirectTo: '/places/discover',
+          pathMatch: 'full',
+        }
+      ]
+    },
+    {
+      path: 'offers',
+      children: [
+        {
+          path:'',
+          loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+        },
+        {
+          path:'new',
+          loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
+        },
+        {
+          path: 'edit/:placeId',
+          loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
+        },
+        {
+          path: ':placeId',
+          loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+        }
+      ],
+    },
+    {
+      path: '',
+      redirectTo: '/places/discover',
+      pathMatch: 'full',
+    }
+  ]
+}
 ];
 
 @NgModule({
